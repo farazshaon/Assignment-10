33ab12875750d9f475caa875ad804236179015e6
faraz shaon <farazshaon0@gmail.com>
2023-08-08 14:58:51 +0600

    ok

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Menu from '../components/Menu'
+
+const page = () => {
+  return (
+    <div>
+     
+      <Menu/>
+      <h1>About Pages</h1>
+    </div>
+  )
+}
+
+export default page
+
---
+++ app/contact/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Menu from '../components/Menu'
+
+const page = () => {
+  return (
+    <div>
+        <Menu/>
+      <h1>Contact Pages</h1>
+    </div>
+  )
+}
+
+export default page
